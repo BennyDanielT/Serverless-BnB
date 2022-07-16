c3dd1510733b97c8d6e3e87c090e46f515122deb
Benny Daniel <benny28dany@gmail.com>
2022-07-16 16:24:10 -0300

    Local Changes

---
+++ bnb-frontend/src/pages/Reports.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar/Navbar";
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1 >
+                Welcome to Serverless BnB! Here're some interesting reports!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
