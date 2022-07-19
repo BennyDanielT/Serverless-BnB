f27e22b208a221e9bfd8591a232eb516bd283e67
Benny Daniel <benny28dany@gmail.com>
2022-07-19 15:41:06 -0300

    Book Rooms Frontend looks satisfactory

--- bnb-frontend/src/components/Rooms/room.js
+++ bnb-frontend/src/components/Rooms/room.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import './roomStyle.css';
+import { reservation } from '../../services/Room/room.service';
 export default function Room(props) {
   const { room } = props;
+  const reservationRequest = {};
+
   // console.log(typeof room.available_on._seconds);
   // console.log(new Date(room.available_on._seconds));
-  let availableDate = new Date(room.available_on._seconds * 1000);
+  // console.log(roomsAPI.data.data);
   // console.log(availableDate);
+
+  let availableDate = new Date(room.available_on._seconds * 1000);
+  const bookThisRoom = async (rm) => {
+    try {
+      const bookRoomResponse = await reservation(reservationRequest);
+      if (bookRoomResponse.success) {
+        // navigate("/notifications");
+      } else {
+        alert('Order not placed, please retry');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
       <img className='item-img-small' src={room.image_url} alt=''></img>
@@ -22,7 +40,7 @@ export default function Room(props) {
           className='item-button'
           // variant='primary'
           size='sm'
-          // onClick={() => onAdd(item)}
+          onClick={() => bookThisRoom(room)}
         >
           {' '}
           Book Now{' '}
