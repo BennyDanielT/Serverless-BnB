d1393a0cb70309c7c508b2451f33dd7f18d32ee4
Benny Daniel <benny28dany@gmail.com>
2022-07-24 12:05:33 -0300

    Integrated User Management with Visualizations

--- bnb-frontend/src/components/Visualizations/Visualization.js
+++ bnb-frontend/src/components/Visualizations/Visualization.js
@@ -1,25 +1,42 @@
 import Navbar from '../Navbar/Navbar';
 import './viz.css';
-const Visualization = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className='viz-header'>
-        <div className='viz-info'>
-          <h2>Welcome to the Visualizations Page</h2>
-          <h3>Here're some interesting charts for your perusal!</h3>
-        </div>
-      </div>
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-      <iframe
-        className='iframe'
-        width='100%'
-        height='920'
-        src='https://datastudio.google.com/embed/reporting/5078cfe8-15f4-4cfd-befd-12e87f04b258/page/kb1xC'
-        allowfullscreen
-      ></iframe>
-    </div>
-  );
+const Visualization = () => {
+  const navigate = useNavigate();
+  const loggedInUser = localStorage.getItem('email');
+  console.log(loggedInUser);
+  if (!loggedInUser) {
+    Swal.fire(
+      'Please Login!',
+      'Only registered members can view visualizations and previous bookings!',
+      'error',
+    );
+    navigate('/loginui');
+  }
+
+  if (loggedInUser) {
+    return (
+      <div>
+        <Navbar />
+        <div className='viz-header'>
+          <div className='viz-info'>
+            <h2>Welcome to the Visualizations Page</h2>
+            <h3>Here're some interesting charts for your perusal!</h3>
+          </div>
+        </div>
+
+        <iframe
+          className='iframe'
+          width='100%'
+          height='920'
+          src='https://datastudio.google.com/embed/reporting/5078cfe8-15f4-4cfd-befd-12e87f04b258/page/kb1xC'
+          allowfullscreen
+        ></iframe>
+      </div>
+    );
+  }
 };
 
 export default Visualization;
